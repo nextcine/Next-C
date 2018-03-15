f94fa9100ce76c01886ebf1f487fa95f0311acc1
miguel <miguel.ruiz@ariadnacg.com>
2018-03-14 22:14:31 -0500

    model comentarios

--- resources/views/video/comments.blade.php
+++ resources/views/video/comments.blade.php
@@ -50,8 +50,41 @@
 				</div>
 					<div class="panel-body">
 						{{ $comment->body }}
+
+							@if(Auth::check() && (Auth::user()->id == $comment->user_id || Auth::user()->id == $video->user_id))
+							<div class="pull-right">
+
+							<!-- Botón en HTML (lanza el modal en Bootstrap) -->
+									<a href="#victorModal{{ $comment->id }}" role="button" class="btn btn-sm btn-primary" data-toggle="modal">Eliminar</a>
+									  
+									<!-- Modal / Ventana / Overlay en HTML -->
+									<div id="victorModal{{ $comment->id }}" class="modal fade">
+									    <div class="modal-dialog">
+									        <div class="modal-content">
+									            <div class="modal-header">
+									                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+									                <h4 class="modal-title">¿Estás seguro?</h4>
+									            </div>
+									            <div class="modal-body">
+									                <p>¿Seguro que quieres borrar este comentario?</p>
+									                <p class="text-warning"><small>{{ $comment->body }}</small></p>
+									            </div>
+									            <div class="modal-footer">
+									                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+									                <button type="button" class="btn btn-danger">Eliminar</button>
+									            </div>
+									        </div>
+									    </div>
+									</div>
+							</div>
+							@endif
 					</div>
+
+
+		
+
 			</div>
+
 		</div>
 		@endforeach
 	</div>	
